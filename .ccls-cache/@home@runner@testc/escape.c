6b1410f236ccd295810cea0db72a8a399716f78a
곽태훈 <76631337+rod40430@users.noreply.github.com>
2023-02-08 00:30:07 +0000

    정수와 부동소수점 수의 오버플로와 언더플로에 대하여

--- .ccls-cache/@home@runner@testc/escape.c
+++ .ccls-cache/@home@runner@testc/escape.c
@@ -2,13 +2,7 @@
 
 int main(void)
 {
-  float salary;
-
-  printf("\a원하는 월급 액수를 입력하시오:");
-  printf(" $______\b\b\b\b\b\b");
-  scanf("%f", &salary);
-  printf("\n\t 월$%.2f이면 연봉으로 $%.2f입니다.", salary, salary * 12);
-  printf("\r와우!\n");
+  
   
   return 0;
 }
