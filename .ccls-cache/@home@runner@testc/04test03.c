347f50047b4bf4840629dc37a91cc49b0cacd24b
곽태훈 <76631337+rod40430@users.noreply.github.com>
2023-02-20 14:21:08 +0000

    4단원 테스트 3번문제

--- .ccls-cache/@home@runner@testc/04test03.c
+++ .ccls-cache/@home@runner@testc/04test03.c
@@ -4,10 +4,7 @@
 
 int main(void)
 {
-  printf("그는 그 그림을 $%2.2f에 팔았다.\n", 2.345e2);
-  printf("%c  %c  %c\n", 'H', 105, '\41');
   printf("\"%s\"\n는 %d 문자이다.\n", Q, strlen(Q));
-  printf("%2.2e은 %2.2f과 같으냐?", 1201.0, 1201.0);
 
   return 0;
 }
