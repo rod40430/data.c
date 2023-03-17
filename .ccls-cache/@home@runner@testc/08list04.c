3ec2a2ee169ef880f1c4e5111b062f412ca1c0bf
xogns <xogns40430@naver.com>
2023-03-17 13:35:52 +0000

    수 맞추기 프로그램 (level up 2)

--- .ccls-cache/@home@runner@testc/08list04.c
+++ .ccls-cache/@home@runner@testc/08list04.c
@@ -4,14 +4,23 @@
 int main(void)
 {
   int guess = 1;
+  char response;
 
   printf("1에서 100까지 범위에 있는 한 정수를 생각하시오.\n");
   printf("내가 추측하지요. 내 추측이 맞으면 y로 응답하고,\n");
   printf("내 추측이 틀리면 n으로 응답하시오.\n");
   printf("당신이 생각한 수가 %d입니까?\n", guess);
-  while (getchar() != 'y')
+  while ((response = getchar()) != 'y')
     {
+      if (response == 'n')
+      {
       printf("그러면 %d입니까?\n", ++guess);
+      }
+      else
+      {
+        printf("미안하지만, y또는 n으로만 응답해 주시오.\n");
+      }
+      
       while (getchar() != '\n')
         {
           continue;
