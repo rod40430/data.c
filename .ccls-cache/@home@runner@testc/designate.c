e8159b5a9837d6f7a92b16d394153f16741df770
xogns <xogns40430@naver.com>
2023-03-21 06:38:56 +0000

    배열의 지정초기화

---
+++ .ccls-cache/@home@runner@testc/designate.c
@@ -0,0 +1,14 @@
+// designate.c -- 지정 초기화자를 사용한다.
+#include <stdio.h>
+#define MONTHS 12
+
+int main(void) {
+  int days[MONTHS] = {31, 28, [4] = 31, 30, 31, [1] = 29};
+  int i;
+
+  for (i = 0; i < MONTHS; i++) {
+    printf("%2d  %d\n", i + 1, days[i]);
+  }
+
+  return 0;
+}
