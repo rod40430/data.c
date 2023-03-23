76243137dadb38096cde6ed66f34761923daa00f
xogns <xogns40430@naver.com>
2023-03-23 14:46:42 +0000

    type()을 알고싶을떄

--- .ccls-cache/@home@runner@testc/zippo2.c
+++ .ccls-cache/@home@runner@testc/zippo2.c
@@ -1,4 +1,4 @@
-// zippo.c -- 포인터 변수를 사용하는 zippo 정보
+// zippo2.c -- 포인터 변수를 사용하는 zippo 정보
 #include <stdio.h>
 int main(void)
 {
@@ -8,18 +8,17 @@ int main(void)
   {1, 3},
   {5, 7}
   };
-  int (*pz)[2];
-  pz = zippo;
+  int (*pz)
 
-  printf("    pz = %p,    pz + 1 = %p\n", pz, pz + 1);
-  printf(" pz[0] = %p, pz[0] + 1 = %p\n", pz[0], pz[0] + 1);
-  printf("   *pz = %p,   *pz + 1 = %p\n", *pz, *pz + 1);
+  printf("    zippo = %p,    zippo + 1 = %p\n", zippo, zippo + 1);
+  printf(" zippo[0] = %p, zippo[0] + 1 = %p\n", zippo[0], zippo[0] + 1);
+  printf("   *zippo = %p,   *zippo + 1 = %p\n", *zippo, *zippo + 1);
 
-  printf("pz[0][0] = %d\n", pz[0][0]);
-  printf("  *pz[0] = %d\n", *pz[0]);
-  printf("    **pz = %d\n", **pz);
-  printf("        pz[2][1] = %d\n", pz[2][1]);
-  printf("*(*(pz + 2) + 1) = %d\n", *(*(pz + 2) + 1));
+  printf("zippo[0][0] = %d\n", zippo[0][0]);
+  printf("  *zippo[0] = %d\n", *zippo[0]);
+  printf("    **zippo = %d\n", **zippo);
+  printf("        zippo[2][1] = %d\n", zippo[2][1]);
+  printf("*(*(zippo + 2) + 1) = %d\n", *(*(zippo + 2) + 1));
 
   return 0;
 }
