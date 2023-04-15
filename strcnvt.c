1600678c2900ba29d1390d7c9af87d4974e81d99
xogns <xogns40430@naver.com>
2023-04-15 00:28:38 +0000

    Initial commit

--- strcnvt.c
+++ strcnvt.c
@@ -7,19 +7,8 @@ char * s_gets(char * st, int n);
 int main(void)
 {
   char number[30];
-  char * end;
-  long value;
-
-  puts("하나의 수를 입력하시오(끝내려면 빈 라인 입력) : ");
-  while (s_gets(number, LIM) && number[0] != '\0')
-    {
-      value = strtol(number, &end, 10);
-      printf("값 : %ld, %s 앞에서 끝났다 (%d)\n", value, end, *end);
-      value = strtol(number, &end, 16);
-      printf("값 : %ld, %s 앞에서 끝났다 (%d)\n", value, end, *end);
-      puts("다음 수를 입력하시오 : ");
-    }
-  puts("안녕!");
+  s_gets(number, LIM);
+  printf("%lf", atof(number));
 
   return 0;
 }
