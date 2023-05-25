85126e0c1af842917f17c54dc526b571937d9847
xogns <xogns40430@naver.com>
2023-05-25 22:51:31 +0900

    Generic  매크로를 사용하는 예시
    전차포훈련 갔다옴 2023-05-19 ~ 25

---
+++ 예제코드/mytype.c
@@ -0,0 +1,26 @@
+// mytype.c
+
+#include <stdio.h>
+
+#define MYTYPE(X) _Generic((X),\
+  int : "int",\
+  float : "float",\
+  double : "double",\
+  default : "other"\
+)
+
+int main(void)
+{
+  int d = 5;
+
+  printf("%s\n", MYTYPE(d));
+  printf("%s\n", MYTYPE(2.0 * d));
+  printf("%s\n", MYTYPE(3L));
+  printf("%s\n", MYTYPE(&d));
+
+  float a = 0.2;
+  printf("%s\n", MYTYPE(a));
+
+
+  return 0;
+}
