1da62a8667e237c52d336760f9d7110f3b365a2f
xogns <xogns40430@naver.com>
2023-05-27 22:29:45 +0900

    converts 직각좌표를 극좌표로 변환한다.

---
+++ 예제코드/rect_pol.c
@@ -0,0 +1,53 @@
+// rect_pol.c -- converts 직각좌표를 극좌표로 변환한다.
+#include <stdio.h>
+#include <math.h>
+
+#define RAD_TO_DEG (180 / (4 * atan(1)))
+
+typedef struct polar_v
+{
+  double magnitude;
+  double angle;
+} Polar_V;
+
+typedef struct rect_v
+{
+  double x;
+  double y;
+} Rect_V;
+
+Polar_V rect_to_polar(Rect_V);
+
+int main(void)
+{
+  Rect_V input;
+  Polar_V result;
+
+  puts("x, y 좌표를 입력하시오(끝내려면 q) : ");
+
+  while (scanf("%lf %lf", &input.x, &input.y) == 2)
+    {
+      result = rect_to_polar(input);
+      
+      printf("magnitude = %0.2f, angle = %0.2f\n",
+        result.magnitude, result.angle);
+    }
+
+  puts("끝.");
+
+  return 0;
+}
+
+Polar_V rect_to_polar(Rect_V rv)
+{
+  Polar_V pv;
+
+  pv.magnitude = sqrt(rv.x * rv.x + rv.y * rv.y);
+
+  if (pv.magnitude == 0)
+    pv.angle = 0.0;
+  else
+    pv.angle = RAD_TO_DEG * atan2(rv.y, rv.x);
+
+  return pv;
+}
